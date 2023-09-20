465bed337e35c84b55009de5df4a7726a9c1fe5b
olilue <o.m.luetolf@gmail.com>
2023-09-20 12:45:59 +0200

    fix wishlist data_adapter tests

--- data_adapter/wishlist.py
+++ data_adapter/wishlist.py
@@ -1,15 +1,46 @@
 from sqlalchemy.orm import Session
+import uuid
 
 from pydantic_models.wishlist import WishlistCreate
 from data_adapter.db_models.wishlist import Wishlist
+from data_adapter.db_models.wishlist_game import WishlistGame
+from data_adapter.db_models.game import Game
 
 def create_new_wishlist(wishlist:WishlistCreate,db:Session):
-    wishlist = Wishlist(**wishlist.model_dump())
-    db.add(wishlist)
+    wishlist_model = wishlist.model_dump()
+    wishlist_model["uuid"] = str(uuid.uuid4())
+    wishlist_entry = Wishlist(**wishlist_model)
+    db.add(wishlist_entry)
     db.commit()
-    db.refresh(wishlist)
-    return wishlist
+    db.refresh(wishlist_entry)
+    return wishlist_entry
 
 def list_wishlists(db:Session):
     wishlists = db.query(Wishlist).all()
     return wishlists
+
+def delete_wishlist(wishlist_uuid:str,db:Session):
+    wishlist = db.query(Wishlist).filter(Wishlist.uuid == wishlist_uuid).first()
+    db.delete(wishlist)
+    db.commit()
+    return wishlist
+
+def get_wishlist(wishlist_uuid:str,db:Session):
+    wishlist = db.query(Wishlist).filter(Wishlist.uuid == wishlist_uuid).first()
+    return wishlist
+
+def update_wishlist(wishlist_uuid:str,wishlist:WishlistCreate,db:Session):
+    wishlist_model = wishlist.model_dump()
+    wishlist_model["uuid"] = wishlist_uuid
+    print(wishlist_model)
+    existing_wishlist = db.query(Wishlist).filter(Wishlist.uuid == wishlist_uuid)
+    if existing_wishlist.first() is None:
+        return False
+    existing_wishlist.update(wishlist_model)
+    db.commit()
+    return True
+
+def get_wishlist_games(wishlist_uuid:str,db:Session):
+    wishlist_games = db.query(WishlistGame).filter(WishlistGame.wishlist_uuid == wishlist_uuid).all()
+    games = db.query(Game).filter(Game.id.in_([wishlist_game.game_id for wishlist_game in wishlist_games])).all()
+    return games
--- tests/data_adapter_tests/test_wishlist.py
+++ tests/data_adapter_tests/test_wishlist.py
@@ -2,25 +2,28 @@ from sqlalchemy.orm import Session
 from datetime import datetime
 
 from pydantic_models.wishlist import WishlistCreate
-from data_adapter.wishlist import create_new_wishlist, list_wishlists
+from data_adapter.wishlist import create_new_wishlist, list_wishlists, delete_wishlist, get_wishlist, update_wishlist
 
-# Write a test for the list_wishlists function
+# Test the create_new_wishlist function and the list_wishlists function
 def test_list_wishlists(db_session:Session):
-    test_time = datetime.now().time()
+    test_time = datetime.now()
+    test_results = []
+    # Create a wishlist
     wishlist = WishlistCreate(
         name="test wishlist",
         email="test@test.com",
         schedule_timestamp=test_time,
         country_code="CH",
     )
-    wishlist = create_new_wishlist(wishlist,db_session)
+    result = create_new_wishlist(wishlist,db_session)
+    test_results.append(result)
+    # Create a second wishlist
     wishlist.name = "test wishlist 2"
     wishlist.email = "test2@test.com"
-    wishlist = create_new_wishlist(wishlist,db_session)
+    result = create_new_wishlist(wishlist,db_session)
+    test_results.append(result)
     # Call the list_wishlists function
     wishlists = list_wishlists(db_session)
-    # Check that the wishlist is in the list
-    assert wishlist in wishlists
     # Check that the list has length 2
     assert len(wishlists) == 2
     # Check that the wishlist has the correct name
@@ -42,4 +45,50 @@ def test_list_wishlists(db_session:Session):
     assert wishlists[0].uuid is not None
     assert wishlists[1].uuid is not None
 
-        
+# Test the delete_wishlist function
+def test_delete_wishlist(db_session:Session):
+    test_time = datetime.now()
+    # Create a wishlist
+    wishlist = WishlistCreate(
+        name="test wishlist",
+        email="test@test.com",
+        schedule_timestamp=test_time,
+        country_code="CH"
+    )
+    result = create_new_wishlist(wishlist,db_session)
+    # ensure that the wishlist is created
+    search_result = get_wishlist(result.uuid,db_session)
+    assert search_result.name == "test wishlist"
+    # Call the delete_wishlist function
+    result = delete_wishlist(result.uuid,db_session)
+    # Check that the wishliste is deleted
+    search_result = get_wishlist(result.uuid,db_session)
+    assert search_result is None
+    
+# Test the update_wishlist function
+def test_update_wishlist(db_session:Session):
+    test_time = datetime.now()
+    # Create a wishlist
+    wishlist = WishlistCreate(
+        name="test wishlist",
+        email="test@test.com",
+        schedule_timestamp=test_time,
+        country_code="CH"
+    )
+
+    result = create_new_wishlist(wishlist,db_session)
+    print(result.uuid)
+    # ensure that the wishlist is created
+    search_result = get_wishlist(result.uuid,db_session)
+    assert search_result.name == "test wishlist"
+    assert search_result.country_code == "CH"
+    # Update the wishlist
+    wishlist.name = "test wishlist 2"
+    wishlist.country_code = "US"
+    update_result = update_wishlist(result.uuid,wishlist,db_session)
+    # Check that the wishlist is updated
+    search_result = get_wishlist(result.uuid,db_session)
+    assert update_result == True
+    assert search_result.name == "test wishlist 2"
+    assert search_result.country_code == "US"
+
